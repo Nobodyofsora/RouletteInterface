f99bfd7dd03f703f8b3ab2a5c4db149a10f862d0
Gaetano <tanocarfi@gmail.com>
2019-06-04 15:51:02 +0200

    fxId

---
+++ Controller.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label cell4;
+
+    @FXML
+    private Label cell5;
+
+    @FXML
+    private Label cell2;
+
+    @FXML
+    private Label cell3;
+
+    @FXML
+    private Label cell8;
+
+    @FXML
+    private Label cell6;
+
+    @FXML
+    private Label cell7;
+
+    @FXML
+    private Label cell1;
+
+    @FXML
+    void 06a814(ActionEvent event) {}
+
+    @FXML
+    void 1a68b2(ActionEvent event) {}
+
+    @FXML
+    void e8e8e8(ActionEvent event) {}
+}
