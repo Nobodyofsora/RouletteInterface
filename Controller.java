cc0f514336e9effdc711953323efce8ed0bf5027
Gaetano Carfì <49553160+tanocarfi@users.noreply.github.com>
2019-06-05 10:29:54 +0200

    Update Controller.java
    Aggiunta dei metodi spin() e choseBet()

--- Controller.java
+++ Controller.java
@@ -1,19 +1,35 @@
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+
+import java.util.ArrayList;
 
 public class Controller {
+    ArrayList<Players> players = new ArrayList<Players>();
+
     @FXML
     private GridPane gridId;
-    
-    @FXML
-    void 06a814(ActionEvent event) {}
 
-    @FXML
-    void 1a68b2(ActionEvent event) {}
+    Players player = new Players("Pippo", 300);
+    Roulette roulette = new Roulette(players);
 
-    @FXML
-    void e8e8e8(ActionEvent event) {}
+    public void spin(){
+        roulette.spinTheWheel();
+        try {
+            roulette.checkWin();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void chooseBet(MouseEvent event) {
+        Label source = (Label) event.getSource();
+        player.setBet(Integer.parseInt(source.getText()));
+        Label source2 = (Label) event.getSource();
+        player.setMoneyBet(Integer.parseInt(source2.getText()));
+    }
 }
