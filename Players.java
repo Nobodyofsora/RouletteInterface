272388a614fab62e6437ee530d2051de8704c8e1
Gaetano <tanocarfi@gmail.com>
2019-08-27 19:20:33 +0200

    Last version

--- Players.java
+++ Players.java
@@ -2,23 +2,33 @@ package sample;
 
 public class Players {
     private String name;
-    public double credit;
+    public int credit;
     private int bet; // odd number: 37/ even number: 38 / number 0-36 / red / black
     private int moneyBet;
     public int win;
-    public boolean completedTurn=true;
+    public boolean completedTurn = false;
 
-    public Players(String name, double credit) {
+    public Players(String name, int
+            credit) {
         this.name = name;
         this.credit = credit;
         this.win=0;
     }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getBet() {
         return bet;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
     }
 
     public void setBet(int bet) {
